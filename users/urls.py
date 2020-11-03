bb3f9fc7dfca6c71b7b4805447f44626f163ad3d
Ilya Verbilo <verbiloilyaa@gmail.com>
2020-11-03 03:00:03 +0300

    added register functionality

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 """Определяет схемы url для пользователей"""
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # включить url авторизации по умолчанию
     path('', include('django.contrib.auth.urls')),
+    # страница регистрации
+    path('register/', views.register, name='register')
 ]
